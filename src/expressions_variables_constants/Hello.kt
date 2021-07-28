aa80e61ed11477892ba35f6144b6d77b1a54d9ba
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2021-07-28 08:31:35 +0100

    updates

--- src/expressions_variables_constants/Hello.kt
+++ src/expressions_variables_constants/Hello.kt
@@ -1,9 +1,8 @@
 package expressions_variables_constants
 
 import java.lang.Math.PI
-import java.lang.Math.sin
 
 fun main() {
     println("Hello World")
-    sin(45 * PI)
+    println(kotlin.math.sin(45 * PI))
 }
