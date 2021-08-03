ccc09b81bf597c5db0405c8e1c39b5f729819f76
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2021-08-03 09:35:30 +0100

    objects

---
+++ src/objects/Object.kt
@@ -0,0 +1,46 @@
+package objects
+
+class Object {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            println("Omo")
+
+            val marie = Student(1, "Marie", "Curie")
+            val mark = Student(2, "Mark", "David")
+            val joshua = Student(3, "Joshua", "Faith")
+
+            StudentRepository.addStudent(marie)
+            StudentRepository.addStudent(mark)
+            StudentRepository.addStudent(joshua)
+
+            StudentRepository.listAllStudents()
+        }
+    }
+}
+
+// Singleton use case
+
+data class Student(val id: Int, val firstName: String, val lastName: String) {
+    var fullName = "$lastName, $firstName"
+}
+
+object StudentRepository {
+    val allStudents = mutableListOf<Student>()
+
+    fun addStudent(student: Student) {
+        allStudents.add(student)
+    }
+
+    fun removeStudent(student: Student) {
+        allStudents.remove(student)
+    }
+
+    fun listAllStudents() {
+        allStudents.forEach {
+            println(it.fullName)
+        }
+    }
+}
